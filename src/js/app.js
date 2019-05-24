fe304dfd7f6415fb5aeb79836c114eab5a8d50a3
Miguel Osorio <mial.osorio@gmail.com>
2019-05-24 10:29:56 +0200

    hit button

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Create and shuffle a new deck of cards
-  const deck = new Deck();
-  deck.shuffle();
-  console.log(deck);
+document.addEventListener("DOMContentLoaded", () => {
+    // Create and shuffle a new deck of cards
+    const deck = new Deck();
+    // const card = new Card(2, "clubs");
+    const hand = document.querySelector("#player-hand");
+    // card.mount(p);
+    deck.shuffle();
+    deck.mount(hand);
+    console.log(deck);
+
+    const hit = document.querySelector(".js-hit");
+    const stand = document.querySelector(".js-stand");
+
+    count = 0;
+
+    hit.addEventListener("click", () => {
+        deck.cards[count].mount(deck.element);
+        count++;
+    });
+
+    // stand.document.addEventListener("click", () => {
+
+    // });
 });
