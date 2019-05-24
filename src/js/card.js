ff11b5b7ff0b72c5400facaf658312ae8040c7c1
Miguel Osorio <mial.osorio@gmail.com>
2019-05-24 09:53:37 +0200

    render and mount

--- src/js/card.js
+++ src/js/card.js
@@ -2,8 +2,25 @@
  * A playing card
  */
 class Card {
-  constructor(rank, suit) {
-    this.rank = rank;
-    this.suit = suit;
-  }
-} 
+    constructor(rank, suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    render() {
+        this.element = document.createElement("div");
+        this.element.innerHTML = "";
+        this.element.className = "card";
+        this.element.style.backgroundImage = `url(img/cards/${this.rank}-of-${this.suit}.svg`;
+    }
+
+    mount(parent) {
+        this.render();
+        parent.appendChild(this.element);
+        this.update();
+    }
+
+    update() {
+      
+    }
+}
